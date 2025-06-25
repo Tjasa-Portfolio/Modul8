54b16ff96545cf45f5dcfa2db018f07c463e6f34
TjasaZil <tjasa.zilavec@gmail.com>
2025-06-25 17:33:16 +0200

    task is done

--- js/listTask.js
+++ js/listTask.js
@@ -1,63 +1,84 @@
-import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
 createApp({
-    data() {
-        return {
-            tasks: [],
-            task : {
-                id : '',
-                name: '',
-                description: '',
-                startDate: '',
-                endDate: '',
-                status:'',
-                priority:''
-            },
-        }
+  data() {
+    return {
+      tasks: [],
+      task: {
+        id: "",
+        name: "",
+        description: "",
+        startDate: "",
+        endDate: "",
+        status: "",
+        priority: "",
+      },
+      openAddFormBoolean: false,
+    };
+  },
+  created() {
+    this.loadAllTasks();
+  },
+  methods: {
+    loadAllTasks() {
+      axios
+        .get("http://localhost:8080/task/getAllTasks")
+        .then((response) => {
+          this.tasks = response.data;
+        })
+        .catch((error) => console.error(error));
     },
-    created(){
-        this.loadAllTasks();
+
+    sendTask() {
+      axios
+        .post("http://localhost:8080/task/addTask", this.task)
+        .then((response) => {
+          this.loadAllTasks();
+          this.task.id = "";
+          this.task.name = "";
+          this.task.description = "";
+          this.task.startDate = "";
+          this.task.endDate = "";
+          this.task.status = "";
+          this.task.priority = "";
+          this.openAddFormBoolean = !this.openAddFormBoolean;
+        })
+        .catch((error) => console.error(error));
     },
-    methods:{
-        loadAllTasks(){
-            axios.get("http://localhost:8080/task/getAllTasks")
-            .then((response) => {
-                this.tasks = response.data;
-            })
-            .catch((error) => console.error(error));
-        },
 
-        sendTask(){
-            axios.post("http://localhost:8080/task/addTask", this.task)
-            .then((response) => {
-                this.loadAllTasks();
-                this.task.id = '';
-                this.task.name = '';
-                this.task.description = '';
-                this.task.startDate = '';
-                this.task.endDate = '';
-                this.task.status = '';
-                this.task.priority = '';
-            })
-            .catch((error) => console.error(error));
-        },
+    updateTask() {
+      axios
+        .put("http://localhost:8080/task/updateTask", this.task)
+        .then((response) => {
+          this.loadAllTasks();
+          this.task.id = "";
+          this.task.name = "";
+          this.task.description = "";
+          this.task.startDate = "";
+          this.task.endDate = "";
+          this.task.status = "";
+          this.task.priority = "";
+        })
+        .catch((error) => console.error(error));
+    },
 
-        deleteEvent(id){
-            axios.delete("http://localhost:8080/task/deleteTask/"+id)
-            .then((response) => {
-                this.loadAllTasks();
-            })
-            .catch((error) => console.error(error));
-        },
-        
-        selectEvent(t){
-            this.task.id = t.id;
-            this.task.name = t.name;
-            this.task.location = t.location;
-            this.task.startDate = t.startDate;
-            this.task.endDate = t.endDate;
-            this.task.status = t.status;
-            this.task.priority = t.priority;
-        }
-    }
-}).mount('#app')
+    deleteTask(id) {
+      axios
+        .delete("http://localhost:8080/task/deleteTask/" + id)
+        .then((response) => {
+          this.loadAllTasks();
+        })
+        .catch((error) => console.error(error));
+    },
+
+    selectTask(t) {
+      this.task.id = t.id;
+      this.task.name = t.name;
+      this.task.description = t.description;
+      this.task.startDate = t.startDate;
+      this.task.endDate = t.endDate;
+      this.task.status = t.status;
+      this.task.priority = t.priority;
+    },
+  },
+}).mount("#app");
