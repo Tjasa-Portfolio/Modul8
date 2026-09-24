b883ab95ce3fbd2b6bc0648187b2fc4a65835529
TjasaZilavec <tjasa.zilavec@netica.si>
2026-09-24 14:13:06 +0200

    #6 added hero component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Navigation from './components/navigation/NavigationComponent.vue'
+import Hero from './components/hero/HeroComponent.vue'
 </script>
 
 <template>
@@ -7,6 +8,7 @@ import Navigation from './components/navigation/NavigationComponent.vue'
     class="flex flex-col justify-start items-center py-[30px] px-2.5 bg-black-main w-full h-screen font-display"
   >
     <Navigation />
+    <Hero />
   </section>
 </template>
 
---
+++ src/components/hero/HeroComponent.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+  <section class="text-white-main"><p>hero</p></section>
+</template>
