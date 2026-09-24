1c30f6a13d5fa33958a2dfdbfce95ab42e3f0dc8
TjasaZilavec <tjasa.zilavec@netica.si>
2026-09-24 12:26:53 +0200

    #5 added navigation mobile functionality

--- src/components/navigation/NavigationComponent.vue
+++ src/components/navigation/NavigationComponent.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import Button from './../shared/ButtonComponent.vue'
+
+const list_items = ref([
+  { id: 1, text: 'Domov' },
+  { id: 2, text: 'Storitve' },
+  { id: 3, text: 'Reference' },
+  { id: 4, text: 'Vizija' },
+])
+const isOpen = ref(false)
+const toggleMenu = () => {
+  isOpen.value = !isOpen.value
+}
 </script>
 <template>
   <nav
@@ -10,11 +22,18 @@ import Button from './../shared/ButtonComponent.vue'
         ><p>ikona</p>
         <p class="font-logo text-white-main font-normal text-[41px]">Modul8</p></span
       >
-      <Button text="test Text" />
+      <Button text="test Text" @click="toggleMenu" />
     </div>
 
-    <ul>
-      <li>to test the font</li>
+    <ul v-if="isOpen" class="flex flex-col justify-center items-end w-full gap-1.5">
+      <li
+        v-for="item in list_items"
+        :key="item.id"
+        class="text-white-main text-lg font-normal hover:text-orange-main hover:font-extrabold hover:border-b-4 hover:border-b-orange-main cursor-pointer"
+      >
+        {{ item.text }}
+      </li>
+      <Button text="Kontakt" />
     </ul>
   </nav>
 </template>
