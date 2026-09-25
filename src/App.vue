a8b710106ddd4b8741a5f81b7cabc02b6dd1df11
TjasaZilavec <tjasa.zilavec@netica.si>
2026-09-25 07:17:18 +0200

    #6 started with hero implementation -  added swiper and a basic sekeleton slider

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-import Navigation from './components/navigation/NavigationComponent.vue'
 import Hero from './components/hero/HeroComponent.vue'
 </script>
 
 <template>
-  <section
-    class="flex flex-col justify-start items-center py-[30px] px-2.5 bg-black-main w-full h-screen font-display"
-  >
-    <Navigation />
+  <section>
     <Hero />
   </section>
 </template>
--- src/components/hero/HeroComponent.vue
+++ src/components/hero/HeroComponent.vue
@@ -1,4 +1,38 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Navigation from '../navigation/NavigationComponent.vue'
+import Button from '../shared/ButtonComponent.vue'
+
+const onSwiper = (swiper) => {
+  console.log(swiper)
+}
+const onSlideChange = () => {
+  console.log('slide change')
+}
+</script>
 <template>
-  <section class="text-white-main"><p>hero</p></section>
+  <section
+    class="flex flex-col justify-start items-center py-[30px] px-2.5 bg-black-main w-full h-screen font-display text-white-main"
+  >
+    <Navigation />
+    <!-- HERO-->
+    <div class="w-full h-screen flex flex-col justify-end items-start gap-[30px]">
+      <h1 class="font-black text-[50px] text-left">Celostne digitalne rešitve na enem mestu</h1>
+      <p class="font-normal text-lg text-left">
+        Premišljeno uporabniško izkušnjo in digitalni razvoj združujemo v celovite rešitve z
+        dolgoročno vrednostjo.
+      </p>
+      <Button text="Začnimo sodelovanje" />
+      <swiper
+        :slides-per-view="3"
+        :space-between="50"
+        @swiper="onSwiper"
+        @slideChange="onSlideChange"
+      >
+        <swiper-slide>Slide 1</swiper-slide>
+        <swiper-slide>Slide 2</swiper-slide>
+        <swiper-slide>Slide 3</swiper-slide>
+        ...
+      </swiper>
+    </div>
+  </section>
 </template>
